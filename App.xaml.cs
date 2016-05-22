0d05795ffb51b1af428200b315ca8c4b7acb871b
janczewskit <janczewskit@op.pl>
2016-05-22 10:56:03 +0200

    example project

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace MVVM_5_Resources
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var culture = new CultureInfo(ConfigurationManager.AppSettings["DefaultCulture"]);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+using Application = System.Windows.Forms.Application;
+
+namespace MVVM_5_Resources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Application.Restart();
+        }
+        
+    }
+}
